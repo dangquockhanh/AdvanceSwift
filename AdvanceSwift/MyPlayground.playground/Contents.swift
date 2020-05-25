a5af43c63d048a9d73bdbaff7e1febda6945ff21
Khanh Dang <dangkhanhios92@gmail.com>
2020-05-25 23:59:29 +0700

    using DispatchSemaphore

--- AdvanceSwift/MyPlayground.playground/Contents.swift
+++ AdvanceSwift/MyPlayground.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var str = "Hello, playground"
+let myQueue = DispatchQueue(label: "myQueue", attributes: .concurrent)
+let semaphore = DispatchSemaphore(value: 5)
+
+for i in 0...14 {
+    myQueue.async {
+        let songNumber = i + 1
+        semaphore.wait()
+        print("Dang tai song: ", songNumber)
+        sleep(2)
+        print("DA tai xong song: ", songNumber)
+        semaphore.signal()
+    }
+}
