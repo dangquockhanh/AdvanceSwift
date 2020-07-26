3161667c89baa45c050aa4429043d1e434bd3585
Khanh Dang <dangkhanhios92@gmail.com>
2020-07-26 12:09:56 +0700

    add project swift UI

---
+++ MasterUI/MasterUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MasterUI
+//
+//  Created by Đặng Khánh  on 7/26/20.
+//  Copyright © 2020 Đặng Khánh . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
