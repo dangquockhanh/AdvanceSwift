cc72d844d0e77410da8e42eb55da4a25c9c41f1c
Khanh Dang <dangkhanhios92@gmail.com>
2020-07-26 14:03:23 +0700

    add myplayground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+/*
+@State được sử dụng cho những kiểu dữ liệu đơn giản, thuộc về một view duy nhất và nên được khai báo private
+ 
+@ObservedObject sử dụng cho những kiểu dữ liệu phức tạp hơn như các class chúng ta định nghĩa và có thể được chia sẻ cho nhiều Views. Đây cũng là kiểu lưu trữ State hữu dụng và sử dụng phổ biết nhất
+ 
+@EnviromentObject sử dụng cho những thuộc tính được khởi tạo ở một nơi nào đó trong App nhưng có thể truy xuất ở mọi nơi
+ 
+@Binding được sử dụng khi muốn đánh dấu giá trị của một biến trong View được cung cấp từ một nơi khác và chia sẻ lẫn nhau
+*/
