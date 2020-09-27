c0c9b1186dc6fe057824cb7f33ebf18d8c7c0523
Khanh Dang <dangkhanhios92@gmail.com>
2020-09-27 11:13:09 +0700

    Filter Image

--- AdvanceSwift/AdvanceSwift/ViewController.swift
+++ AdvanceSwift/AdvanceSwift/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupView()
     }
-
-
+    
+    func setupView() {
+        guard let image = photoImage.image, let cging = image.cgImage else {
+            print("imageView doen't have an Image")
+            return
+        }
+        
+        let coreImage = CIImage(cgImage: cging)
+        
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(coreImage, forKey: kCIInputImageKey)
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        
+        if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage {
+            let filteredImage = UIImage(ciImage: output)
+            photoImage?.image = filteredImage
+        } else {
+            print("image filtering failed")
+        }
+    }
 }
-
