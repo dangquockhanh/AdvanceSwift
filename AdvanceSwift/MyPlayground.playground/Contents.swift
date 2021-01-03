05f3aa0a9060465f7ac7807201ca3c4d089cff9d
Khanh Dang <dangkhanhios92@gmail.com>
2021-01-03 17:46:40 +0700

    commit 1

--- AdvanceSwift/MyPlayground.playground/Contents.swift
+++ AdvanceSwift/MyPlayground.playground/Contents.swift
@@ -1,15 +1,28 @@
 import UIKit
 
+var numbers = [1, 2, 20, 20, 20]
 
-import Foundation
+// Tính tổng các số lẻ
+func sumDecimal(numbers: [Int]) -> Int {
+    var a = [Int]()
+    var sum = 0
+    for i in numbers {
+        if i % 2 != 0 {
+            a.append(i)
+        }
+    }
+    sum = a.reduce(0, +)
+    return sum
+}
 
-class Person {
-    let name: String
-    init(name: String) {
-        self.name = name
-        print("\(name) is being initialized")
+sumDecimal(numbers: numbers)
+
+//Sắp xếp lại 1 dãy số từ dưới lên trên
+func reverseNumber(_ numebrs: [Int]) -> [Int] {
+    var reverse = [Int]()
+    for i in numebrs {
+        reverse.append(i)
     }
-    deinit {
-        print("\(name) is being deinitialized")
-    }
+    
+    return reverse.reversed()
 }
