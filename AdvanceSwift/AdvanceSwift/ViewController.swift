6b3d16464c827a94a74372a2c7d17d9e781811c9
Khanh Dang <dangkhanhios92@gmail.com>
2021-02-06 11:46:06 +0700

    commit

--- AdvanceSwift/AdvanceSwift/ViewController.swift
+++ AdvanceSwift/AdvanceSwift/ViewController.swift
@@ -8,12 +8,42 @@
 
 import UIKit
 import Foundation
+import Combine
 
 class ViewController: UIViewController {
     
+
+    var subscriptions = Set<AnyCancellable>()
+    var countPublisher = CurrentValueSubject<Int, Never>(0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+        countPublisher.value = 1
         
+        let newValue = countPublisher.value + 1
+        countPublisher.send(newValue)
     }
+    
+    func setup() {
+        countPublisher
+              .print("Publisher:" )
+              .sink(receiveCompletion: { (completion) in
+                  print(completion)
+                }) { (value) in
+                  print(value)
+                }
+              .store(in: &subscriptions)
+    }
+    
+    @IBAction func increase(_ sender: Any) {
+       countPublisher.value += 1
+       
+     }
+     
+     @IBAction func reduce(_ sender: Any) {
+       countPublisher.value -= 1
+     }
+    
     
 }
