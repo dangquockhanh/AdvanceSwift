e7b37abf507e2d2bf991aa47bc328b1889c57824
Khanh Dang <dangkhanhios92@gmail.com>
2021-02-06 12:23:35 +0700

    Done switch change tableView

---
+++ AdvanceSwift/AdvanceSwift/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  AdvanceSwift
+//
+//  Created by Đặng Khánh  on 2/6/21.
+//  Copyright © 2021 Đặng Khánh . All rights reserved.
+//
+
+import Foundation
+
+class Constant {
+    static let tableViewCell = "TableViewCell"
+}
--- AdvanceSwift/AdvanceSwift/ViewController.swift
+++ AdvanceSwift/AdvanceSwift/ViewController.swift
@@ -11,9 +11,60 @@ import Foundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var segmentButton: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var isShowing = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupView()
     }
     
+    func setupView() {
+        registerTableView()
+    }
+    
+    func registerTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: Constant.tableViewCell, bundle: nil), forCellReuseIdentifier: Constant.tableViewCell)
+    }
+}
+
+//MARK: - ACTIONS
+extension ViewController {
+    
+    @IBAction func selected_segment(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            isShowing = false
+            tableView.reloadData()
+        case 1:
+            isShowing = true
+            tableView.reloadData()
+        default:
+            break
+        }
+    }
+}
+
+//MARK: -UITABLEVIEW
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !isShowing {
+            return 10
+        }
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Constant.tableViewCell, for: indexPath) as? TableViewCell else { return UITableViewCell() }
+        if !isShowing {
+            cell.textLabel?.text = "Trái"
+        } else {
+            cell.textLabel?.text = "Phải"
+        }
+        return cell
+    }
 }
