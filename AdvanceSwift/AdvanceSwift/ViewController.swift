c26093cae01ad2b6864e610f6df51c8b43e07e5c
Khanh Dang <dangkhanhios92@gmail.com>
2021-02-08 21:37:03 +0700

    create radios button

--- AdvanceSwift/AdvanceSwift/ViewController.swift
+++ AdvanceSwift/AdvanceSwift/ViewController.swift
@@ -9,11 +9,36 @@
 import UIKit
 import Foundation
 
+enum ButtonType: Int {
+    case one 
+    case two
+    case three
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var selectedButton: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    @IBAction func selected_actions(_ sender: UIButton) {
+        selectedButton.forEach { (
+            $0.setImage(UIImage(named: "ic_unSelected"), for: .normal)
+        )}
+        sender.setImage(UIImage(named: "ic_selected"), for: .normal)
+        let index = getIndexButton()
+        print(index)
+    }
+    
+    func getIndexButton() -> ButtonType {
+        for (index, button) in selectedButton.enumerated() {
+            if button.imageView?.image == UIImage(named: "ic_selected") {
+                return ButtonType.init(rawValue: index) ?? .one
+            }
+        }
+        return .one
+    }
 }
