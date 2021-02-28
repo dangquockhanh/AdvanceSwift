8e61e8d1583fc9aafdde771373e9e3100e38c33e
Khanh Dang <dangkhanhios92@gmail.com>
2021-02-28 21:44:41 +0700

    init

--- AdvanceSwift/MyPlayground.playground/Contents.swift
+++ AdvanceSwift/MyPlayground.playground/Contents.swift
@@ -1,4 +1,14 @@
 import UIKit
+import Combine
 
+let array = [1, 2, 3, 4, 5].publisher
 
+let subscriber = Set<AnyCancellable>()
 
+let pass = PassthroughSubject<Int, Never>()
+
+pass.send(1)
+pass.send(2)
+pass.sink { subscriber in
+    <#code#>
+}
